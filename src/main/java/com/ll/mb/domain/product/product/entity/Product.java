00a245fc53a0303437cd11d3afe9c94d9045fdd8
hagd <hagd0520@gmail.com>
2023-12-24 15:36:30 +0900

    book

---
+++ src/main/java/com/ll/mb/domain/product/product/entity/Product.java
@@ -0,0 +1,25 @@
+package com.ll.mb.domain.product.product.entity;
+
+import com.ll.mb.domain.member.member.entity.Member;
+import com.ll.mb.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+@Builder
+@ToString(callSuper = true)
+public class Product extends BaseEntity {
+    @ManyToOne
+    private Member maker;
+    private String relTypeCode;
+    private long relId;
+    private String name;
+    private int price;
+}
